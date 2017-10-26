b76dbc25a4db1589b8c0b1bc6fafd70dc36848e0
Matt Blair <blair1618@gmail.com>
2017-10-26 17:01:51 -0400

    Add scripts for intro sequence

---
+++ Assets/Game/Scripts/IntroCameraOrbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCameraOrbit : MonoBehaviour {
+
+    public GameObject OrbitTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(OrbitTarget.transform.position, Vector3.up, 0.03f);
+	}
+}
---
+++ Assets/Game/Scripts/IntroSequence.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSequence : MonoBehaviour {
+
+    public GameObject IntroObject;
+
+    private MonoBehaviour ballController;
+    private MonoBehaviour thirdPersonCamera;
+
+	// Use this for initialization
+	void Start () {
+        var player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            ballController = player.GetComponent<BallController>();
+            if (ballController != null)
+            {
+                ballController.enabled = false;
+            }
+
+            thirdPersonCamera = GetComponent<ThirdPersonCamera>();
+            if (thirdPersonCamera != null)
+            {
+                thirdPersonCamera.enabled = false;
+            }
+        }
+
+        if (IntroObject != null)
+        {
+            this.transform.SetParent(IntroObject.transform);
+            this.transform.localPosition = Vector3.zero;
+            this.transform.localRotation = Quaternion.identity;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if (ballController != null)
+            {
+                ballController.enabled = true;
+            }
+
+            if (thirdPersonCamera != null)
+            {
+                thirdPersonCamera.enabled = true;
+            }
+
+            if (IntroObject != null)
+            {
+                IntroObject.SetActive(false);
+            }
+            this.transform.SetParent(null);
+        }
+	}
+}
