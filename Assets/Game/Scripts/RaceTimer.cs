36357d3fd4e5bff4a02372cb37e1e99606e0473d
Matt Blair <blair1618@gmail.com>
2017-10-27 13:23:16 -0400

    Add RaceTimer to add to a Text for time display

---
+++ Assets/Game/Scripts/RaceTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceTimer : MonoBehaviour {
+
+    public float SecondsRemaining = 100;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start()
+    {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        SecondsRemaining = Mathf.Max(SecondsRemaining - Time.deltaTime, 0);
+
+        int minutes = Mathf.FloorToInt(SecondsRemaining / 60);
+        int seconds = Mathf.FloorToInt(SecondsRemaining % 60);
+        int centiseconds = Mathf.FloorToInt((SecondsRemaining * 100) % 100);
+        text.text = string.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, centiseconds);
+	}
+}
