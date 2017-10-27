c7b84b3ade0e0550518baf5457decfb52c4fca00
Matt Blair <blair1618@gmail.com>
2017-10-27 12:24:42 -0400

    Add script to launch the player into the air on target collision

---
+++ Assets/Game/Scripts/LaunchPlayerOnCollide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPlayerOnCollide : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            collision.rigidbody.AddForce(Vector3.up * 50, ForceMode.Impulse);
+        }
+    }
+}
