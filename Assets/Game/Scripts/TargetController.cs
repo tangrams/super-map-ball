506216fed8fde4304672c2a7820b897a397b8081
Matt Blair <blair1618@gmail.com>
2017-10-31 01:24:09 -0700

    (OnPlayerCollide + TargetTracker) -> TargetController

--- Assets/Game/Scripts/TargetController.cs
+++ Assets/Game/Scripts/TargetController.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnPlayerCollide : MonoBehaviour {
+public class TargetController : MonoBehaviour {
 
     public GameObject FinishUIObject;
 
     public GameObject RaceTimerObject;
 
+    public GameObject[] StartPositions;
+
     private GameObject player;
     private FinishUI finishUI;
     private RaceTimer raceTimer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
         if (FinishUIObject != null)
         {
@@ -24,11 +26,13 @@ public class OnPlayerCollide : MonoBehaviour {
         {
             raceTimer = RaceTimerObject.GetComponent<RaceTimer>();
         }
-	}
-	
+        ResetPosition();
+    }
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * 2);
 	}
 
     void OnCollisionEnter(Collision collision)
@@ -43,7 +47,16 @@ public class OnPlayerCollide : MonoBehaviour {
             {
                 raceTimer.IsRunning = false;
             }
+            ResetPosition();
+        }
+    }
 
+    public void ResetPosition()
+    {
+        var start = StartPositions[Random.Range(0, StartPositions.Length)];
+        if (start != null)
+        {
+            transform.position = start.transform.position;
         }
     }
 }
